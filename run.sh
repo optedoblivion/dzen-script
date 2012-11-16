836db3926e9eda9e688a9ad9ebf3e20a3dd8eb1b
Martin <optedoblivion@gmail.com>
2012-11-16 01:05:54 -0800

    Laptop version almost finished

--- run.sh
+++ run.sh
@@ -5,8 +5,9 @@ BG='#111111'
 FONT='-*-clean-*-*-*-*-12-*-*-*-*-*-*-*'
 SEP="  "
 GDBW="20"
-GDBH="5"
-ICONPATH="$HOME/.dzen2/xbm"
+GDBH="10"
+MODULEPATH="$HOME/.dzen2/modules"
+ICONPATH="$HOME/.dzen2/assets/xbm"
 NOTIFYICONPATH="$HOME/.dzen2/notify_icons"
 NORMALCOLOR="^fg()"
 ERRORCOLOR="^fg(red)"
@@ -16,51 +17,45 @@ COOLTEMPCOLOR="^fg(blue)"
 HOTTEMPCOLOR="^fg(red)"
 
 myVol(){
-    ismute=`amixer get Master|grep %|awk '{ print $6 }'|sed 's/\[//g'|sed 's/\]//g'`
+    ismute=$(amixer get Master|grep %|awk '{ print $6 }'|sed 's/\[//g'|sed 's/\]//g')
     if [ "$ismute" == "off" ]; then
         VBS="0"
         VICO="^i($ICONPATH/spkr_02.xbm)" 
         COLOR="$ERRORCOLOR"
     else
-        VBS=`amixer get Master|grep %|awk '{ print $4 }'|sed 's/%//g'|sed 's/\[//g'|sed 's/\]//g'`
+        VBS=$(amixer get Master|grep %|awk '{ print $4 }'|sed 's/%//g'|sed 's/\[//g'|sed 's/\]//g')
         VICO="^i($ICONPATH/spkr_01.xbm)" 
     fi
 
-    VBAR=`echo "$VBS" | gdbar -fg '#aecf96' -bg gray40 -h $GDBH -w $GDBW|awk '{ print $1 }'`
+    VBAR=$(echo "$VBS" | gdbar -s v -fg "$FG" -bg gray40 -h $GDBH -w $GDBW|awk '{ print $1 }')
     echo "$COLOR$VICO $VBAR$CLEARCOLOR"
 
 }
 
 myBat(){
-    MAX=`cat /proc/acpi/battery/BAT0/info|grep last|awk '{ print $4 }'`
-    CHARGE=`cat /proc/acpi/battery/BAT0/state|grep remaining|awk '{ print $3 }'`
     TIME=""
-    STATE=`cat /proc/acpi/battery/BAT0/state |grep charging|awk '{ print $3 }'`
-    if [ "$STATE" != "discharging" ]; then
-        RESULT=`echo $CHARGE|gdbar -w $GDBW -h $GDBH -max $MAX`
-        BAR=`echo $RESULT|awk '{ print $2 }'`
-        PERCENT=`echo $RESULT|awk '{ print $1 }'`
+    STATE=$(acpi | awk '{ print $3 }' | sed s/,//g)
+    if [ "$STATE" != "Discharging" ]; then
+        PERCENT=$(acpi | awk ' {print $4 }' | sed s/,//g)
         ICO="^i($ICONPATH/ac_01.xbm)"
     else
-        RESULT=`echo $CHARGE|gdbar -w $GDBW -h $GDBH -max $MAX`
-        BAR=`echo $RESULT|awk '{ print $2 }'`
-        PERCENT=`echo $RESULT|awk '{ print $1 }'`
-        NUM=`echo $PERCENT|sed s/\%//`
-        if [ "$NUM" -gt "50" ]; then
+        PERCENT=$(acpi | awk ' {print $4 }' | sed s/,//g)
+        NUM=$(echo $PERCENT|sed s/\%//)
+        if [ "$NUM" -gt "75" ]; then
             ICO="^i($ICONPATH/bat_full_01.xbm)"
-            BATTERY_CRITICAL=`ls /tmp|grep battery_critical`
+            BATTERY_CRITICAL=$(ls /tmp|grep battery_critical)
             if [ "$BATTERY_CRITICAL" != "" ]; then
                 rm /tmp/battery_critical
             fi
         elif [ "$NUM" -gt "20" ]; then
             ICO="^i($ICONPATH/bat_low_01.xbm)"
-            BATTERY_CRITICAL=`ls /tmp|grep battery_critical`
+            BATTERY_CRITICAL=$(ls /tmp|grep battery_critical)
             if [ "$BATTERY_CRITICAL" != "" ]; then
                 rm /tmp/battery_critical
             fi
         else
             ICO="^i($ICONPATH/bat_empty_01.xbm)"
-            BATTERY_CRITICAL=`ls /tmp|grep battery_critical`
+            BATTERY_CRITICAL=$(ls /tmp|grep battery_critical)
             if [ "$BATTERY_CRITICAL" == "" ]; then
                 notify-send "Battery Critical" "You battery state is critical $PERCENT left."
                 touch /tmp/battery_critical
@@ -73,24 +68,29 @@ myBat(){
 }
 
 myMem(){
-    MAX=`free -m|grep Mem|awk '{ print $2 }'`
-    USE=`free -m|grep Mem|awk '{ print $3 }'`
-    BAR=`echo $USE|gdbar -w $GDBW -h $GDBH -max $MAX|awk '{ print $1 }'`
-    VICO="^i($ICONPATH/mem.xbm)"
-    if [ `echo $BAR|cut -d '%' -f 1` -gt 90 ]; then
+    MAX=$(free -m | grep Mem | awk '{ print $2 }')
+    USE=$(free -m | grep Mem | awk '{ print $3 }')
+    PLUSMINUS=$(free -m | grep "\-/+" | awk '{ print $3 }')
+    USE=$(echo "$USE - $PLUSMINUS" | bc -q 2>/dev/null)
+    PERCENT=$(echo "scale=2; ($USE/$MAX) * 100" | bc -q 2>/dev/null)
+    PERCENT=$(echo "$PERCENT" | cut -d '.' -f 1)
+    ICO="^i($ICONPATH/mem.xbm)"
+    if [ $PERCENT -gt 90 ]; then
         COLOR="$ERRORCOLOR"
     fi
-    echo "$COLOR$VICO $BAR$CLEARCOLOR"
+    PERCENT="$PERCENT%"
+    echo "$COLOR$ICO $PERCENT$CLEARCOLOR"
 }
 
 myCPU(){
     ICO="^i($ICONPATH/cpu.xbm)"
-    CPU=`gcpubar -c 2 |awk '{ print $2 }'`
-    CPU=`echo $CPU|awk '{ print $2 }'`
-    if [ `echo $CPU|cut -d '%' -f 1` -gt 90 ]; then
+    PERCENTAGE=$("$MODULEPATH/cpubar")
+    PERCENTAGE=$(echo "$PERCENTAGE" | tr -d " ")
+    NUM=$(echo $PERCENTAGE | cut -d '%' -f 1)
+    if [ $NUM -gt 90 ]; then
         COLOR="$ERRORCOLOR"
     fi
-    echo "$COLOR$ICO $CPU$CLEARCOLOR"
+    echo "$COLOR$ICO $PERCENTAGE$CLEARCOLOR"
 }
 
 myDisks(){
@@ -118,21 +118,23 @@ myDisks(){
 }
 
 myTemp(){
-    MAX=`sensors|grep temp1|awk '{ print $5 }'|cut -d '.' -f 1|sed s/+//g`
-    TMP=`sensors|grep temp1|awk '{ print $2 }'|cut -d '.' -f 1|sed s/+//g`
-    BAR=`echo $TMP|gdbar -w $GDBW -h $GDBH -max $MAX|awk '{ print $1 }'`
+    MAX=$(sensors|grep temp1|awk '{ print $6 }'|cut -d '.' -f 1|sed s/+//g)
+    TMP=$(sensors|grep temp1|awk '{ print $2 }'|cut -d '.' -f 1|sed s/+//g)
+    PERCENT=$(echo "scale=2; ($TMP/$MAX) * 100" | bc  )
+    PERCENT=$(echo "$PERCENT" | cut -d '.' -f 1)
     ICO="^i($ICONPATH/temp.xbm)"
     COLOR="$COOLTEMPCOLOR"
-    if [ `echo $BAR|sed s/\%//` -gt 90 ]; then
+    if [ $PERCENT -gt 90 ]; then
         notify-send "Temperature Critical" "Cool this bitch down."
         COLOR="$HOTTEMPCOLOR"
     fi
     echo "$COLOR$ICO $TMP°C$CLEARCOLOR"
 }
+
 while true ; do
     dt=`date +"%I:%M %a, %x"`
-    echo "$(myDisks)$SEP$(myCPU)$SEP$(myMem)$SEP$(myVol)$SEP$(myTemp)$SEP$(myBat)$SEP$dt"
+    #echo "$(myDisks)$SEP$(myCPU)$SEP$(myMem)$SEP$(myVol)$SEP$(myTemp)$SEP$(myBat)$SEP$dt"
+    echo "$(myVol)$SEP$(myCPU)$SEP$(myMem)$SEP$(myBat)$SEP$(myTemp)$SEP$dt"
     sleep 1
 done | dzen2 -h '16' -ta r -fg $FG -bg $BG -fn $FONT 
-
 
